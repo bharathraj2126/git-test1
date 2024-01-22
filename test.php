5e0c599efbd3f7ff914d4a43edc4e74510fbf625
bharathraj2126 <bharathraj2126@gmail.com>
2024-01-22 10:33:35 -0500

    #My first commit

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+
+// Simple PHP code for multiplication
+
+// Define two numbers
+$number1 = 5;
+$number2 = 10;
+
+// Perform multiplication
+$result = $number1 * $number2;
+
+// Display the result
+echo "The result of multiplication is: " . $result;
+
+?>
